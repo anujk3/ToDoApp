480d7bd31a808de26481eab2c00b76c305eb70c7
Anuj Katiyal <anujk3@gmail.com>
2014-09-21 00:28:43 +0530

    First time made changes to the AppDelegate itself... Made changes in the main application delegate that is.

--- ToDoApp/AppDelegate.m
+++ ToDoApp/AppDelegate.m
@@ -51,4 +51,18 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
+    UIApplicationState state = [application applicationState];
+    if (state == UIApplicationStateActive){
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Reminder" message:notification.alertBody delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+    //Request to reload table view data
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"reloadData" object:self];
+    
+    //Set icon badge number to 0
+    application.applicationIconBadgeNumber = 0;
+    
+}
+
 @end
